eb0e36cc35518b0b4601d5423c4fd8c683fa16ad
kenevy <wc106916>
2020-09-23 22:20:30 +0800

    feat: send email

--- App/ext.py
+++ App/ext.py
@@ -1,13 +1,16 @@
 # 扩展库初始化
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 
 models = SQLAlchemy()
 migrate = Migrate()
+mail = Mail()
 
 
 def init_ext(app):
     models.init_app(app)
     migrate.init_app(app, models)
+    mail.init_app(app)
     Session(app)
--- App/models/system_user.py
+++ App/models/system_user.py
@@ -1,5 +1,7 @@
 import time
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from App.ext import models
 
 
@@ -7,11 +9,25 @@ class SystemUser(models.Model):
     user_id = models.Column(models.Integer, primary_key=True)
     user_name = models.Column(models.String(50), nullable=False)
     user_avatar = models.Column(models.String(255), default='')
-    password = models.Column(models.String(255), nullable=False)
+    _password = models.Column(models.String(255), nullable=False)
     create_at = models.Column(models.Integer)
     update_at = models.Column(models.Integer, default=int(time.time()))
     authority = models.Column(models.String(255), default='{}')
     is_valid = models.Column(models.Boolean, default=True)
+
+    # 将方法变成属性， 并禁止获取
+    @property
+    def password(self):
+        raise Exception("Error Action: Password can't access")
+
+    # 自动加密
+    @password.setter
+    def password(self, value):
+        self._password = generate_password_hash(value)
+
+    # 封装校验密码的方法
+    def check_password(self, password):
+        return check_password_hash(self._password, password)
 
     def save(self):
         models.session.add(self)
