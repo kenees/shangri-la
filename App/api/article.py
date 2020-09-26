a7b75da65863eb2a077eadc2435465b8af610c89
kenevy <my106916@outlook.com>
2020-09-26 23:56:47 +0800

    update: update the article models, add crud

---
+++ App/api/article.py
@@ -0,0 +1,134 @@
+import time
+
+from flask import request
+from flask_restful import Resource, fields, marshal_with, marshal
+
+from App.models import Article
+
+article_fields = {
+    "id": fields.Integer(attribute='article_id'),
+    "article_name": fields.String,
+    "article_tag": fields.String,
+    "create_at": fields.Integer,
+    "update_at": fields.Integer,
+    "reading_number": fields.Integer,
+    "edit_user": fields.String,
+    "comment_number": fields.Integer,
+}
+
+single_list_fields = {
+    "remark": fields.String,
+    "success": fields.Boolean,
+    "data": fields.Nested(article_fields)
+}
+
+
+class ArticleResource(Resource):
+
+    @marshal_with(single_list_fields)
+    def get(self):
+        article_list = Article.query.all()
+        print('article_list', article_list)
+        return {
+            "remark": "get success",
+            "success": True,
+            "data": article_list,
+        }
+
+    def put(self):
+
+        date = request.json
+        if date is None or date.get("article_id") is None:
+            return {
+                "remark": "参数错误",
+                "success": False,
+            }
+
+        article = Article.query.get_or_404(date.get("article_id"))
+        print('article', article)
+        if not article:
+            return {
+                "remark": "文章不存在",
+                "success": False,
+            }
+
+        article_name = date.get('article_name')
+        article_tag = date.get("article_tag")
+        edit_user = date.get("edit_user")
+        print(article_name, article_tag, edit_user)
+        if article_name is not None:
+            article.article_name = article_name
+        if article_tag is not None:
+            article.article_tag = article_tag
+        if edit_user is not None:
+            article.edit_user = edit_user
+        article.update_at = int(time.time())
+
+        if not article.save():
+            return {
+                "remark": "更新失败",
+                "success": False,
+            }
+
+        return {
+            "remark": "更新成功",
+            "success": True,
+            "data": marshal(article, article_fields)
+        }
+
+    def post(self):
+
+        date = request.json
+        if date is None:
+            return {"msg": "参数不能为空"}
+        elif date.get('article_name') is None:
+            return {"msg": "article_name不能为空"}
+        elif date.get('article_content') is None:
+            return {"msg": "article_content不能为空"}
+        elif date.get('edit_user') is None:
+            return {"msg": "edit_user不能为空"}
+
+        article_name = date.get('article_name')
+        article_content = date.get("article_content")
+        edit_user = date.get("edit_user")
+        article_tag = date.get("article_tag")
+
+        article = Article()
+        article.article_name = article_name
+        article.article_content = article_content
+        article.edit_user = edit_user
+        if article_tag is not None:
+            article.article_tag = article_tag
+        article.create_at = int(time.time())
+
+        if not article.save():
+            return {
+                "remark": "创建失败",
+                "success": False,
+            }
+
+        return {
+            "remark": "创建成功",
+            "success": True,
+            "data": marshal(article, article_fields)
+        }
+
+    def delete(self, id):
+
+        article = Article.query.get_or_404(id)
+
+        if not article:
+            return {
+                "remark": "tag is not found",
+                "success": False,
+            }
+        if not article.delete():
+            return {
+                "remark": "delete error",
+                "success": False,
+            }
+
+        return {
+            "remark": "delete success",
+            "success": True,
+        }
--- App/models/article.py
+++ App/models/article.py
@@ -4,13 +4,33 @@ from App.ext import models
 class Article(models.Model):
     article_id = models.Column(models.Integer, primary_key=True)
     article_name = models.Column(models.String(255))
+    article_content = models.Column(models.Text)
     article_tag = models.Column(models.String(255))
-    create_at = models.Column(models.DateTime)
-    update_at = models.Column(models.DateTime)
+    create_at = models.Column(models.Integer)
+    update_at = models.Column(models.Integer, default=create_at)
     reading_number = models.Column(models.Integer, default=0)
     edit_user = models.Column(models.String(50))
     comment_number = models.Column(models.Integer, default=0)
 
     def save(self):
-        models.session.add(self)
-        models.session.commit()
+        try:
+            models.session.add(self)
+            models.session.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    #
+    # def update_tag(tag, attr):
+    #     tag.update(attr)
+    #     models.session.commit()
+
+    def delete(self):
+        try:
+            models.session.delete(self)
+            models.session.commit()
+            return True
+        except Exception as e:
+            print(e)
+            return False
